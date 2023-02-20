a849b743dd6486b0fce9c82c64f6e32d3771febe
kahmoh <joshmcfadden17@gmail.com>
2023-02-20 17:40:35 +0000

    add background image and title and change container sizes to work on multiple devices

--- main.js
+++ main.js
@@ -4,17 +4,16 @@ function createGrid(rows,cols) {
     CONTAINER.style.display = 'grid';
     CONTAINER.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
     CONTAINER.style.gridAutoRows = '1fr';
-    CONTAINER.style.width = '300px';
-    CONTAINER.style.height = '300px';
+
+    const viewportSize = Math.min(window.innerWidth, window.innerHeight);
+    CONTAINER.style.width = `${viewportSize}px`;
+    CONTAINER.style.height = `${viewportSize}px`;
+
 
     for (let i = 0; i < rows * cols; i++) {
         const cell = document.createElement('div');
 
-        cell.style.backgroundColor = '#fff';
-
-        cell.addEventListener('mouseover', (event) => {
-            event.target.classList.add('hover')
-        });
+        CONTAINER.style.backgroundColor = '#fff';
 
         cell.addEventListener('mouseover', (event) => {
             event.target.style.backgroundColor = '#000'
